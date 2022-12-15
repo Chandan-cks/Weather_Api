b5771506a0e68000d5895116c2845c4811a090f5
chandan kumar sahoo <77827600+Chandan-cks@users.noreply.github.com>
2022-12-16 01:18:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import Payment from './Payment/payment';
+import App from './App2.jsx';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {/* <Payment /> */}
+    <App />
+  </React.StrictMode>
+);
